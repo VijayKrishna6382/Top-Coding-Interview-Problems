6be850e575a02b78ac69fc99448ff0a33b1bdf04
VijayKrishna6382 <144125254+VijayKrishna6382@users.noreply.github.com>
2026-03-17 07:08:27 +0530

    Create Pascal's First method.java
    Given two integers r and c, return the value at the rth row and cth column (1-indexed) in a Pascal's Triangle.
    
    In Pascal's triangle:
    
    The first row contains a single element 1.
    
    Each row has one more element than the previous row.
    
    Every row starts and ends with 1.
    
    For all interior elements (i.e., not at the ends), the value at position (r, c) is computed as the sum of the two elements directly above it from the previous row:
    
    Pascal[r][c]=Pascal[r−1][c−1]+Pascal[r−1][c]
    where indexing is 1-based
    
    Example 1
    
    Input: r = 4, c = 2
    
    Output: 3
    
    Explanation:
    
    The Pascal's Triangle is as follows:
    
    1
    
    1 1
    
    1 2 1
    
    1 3 3 1
    
    ....
    
    Thus, value at row 4 and column 2 = 3
    
    Example 2
    
    Input: r = 5, c = 3
    
    Output: 6
    
    Explanation:
    
    The Pascal's Triangle is as follows:
    
    1
    
    1 1
    
    1 2 1
    
    1 3 3 1
    
    1 4 6 4 1
    
    ....
    
    Thus, value at row 5 and column 3 = 6

---
+++ Pascal's Triangle/Pascal's First method.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pascalTriangleI(int r, int c) {
+          return pt(r-1,c-1);
+    }
+    static int pt(int r,int c){
+        int ans=1;
+        for(int i=0;i<c;i++){
+            ans=ans*(r-i);
+            ans=ans/(i+1);
+        }
+        return ans;
+    }
+}

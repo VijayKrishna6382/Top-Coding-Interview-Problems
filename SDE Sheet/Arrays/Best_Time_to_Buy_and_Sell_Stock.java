f5919ab3d8f9ab3cea5dbe4b37486668f86fa11c
VijayKrishna6382 <144125254+VijayKrishna6382@users.noreply.github.com>
2026-07-15 22:54:13 +0530

    change

--- SDE Sheet/Arrays/Best_Time_to_Buy_and_Sell_Stock.java
+++ SDE Sheet/Arrays/Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,10 +1,33 @@
 package sdesheet;
 
-public class Best_Time_to_Buy_and_Sell_Stock {
+import java.util.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Best_Time_to_Buy_and_Sell_Stock {
 
-	}
+    public int maxProfit(int[] prices) {
+        int min=prices[0];
+        int profit=0;
 
+        for(int i=1;i<prices.length;i++){
+            int cost=prices[i]-min;
+            profit=Math.max(profit,cost);
+            min=Math.min(min,prices[i]);
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int[] prices=new int[n];
+
+        for(int i=0;i<n;i++){
+            prices[i]=sc.nextInt();
+        }
+
+        Best_Time_to_Buy_and_Sell_Stock obj=new Best_Time_to_Buy_and_Sell_Stock();
+        System.out.println(obj.maxProfit(prices));
+    }
 }
